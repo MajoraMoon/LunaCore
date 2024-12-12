65b1eb4db8bd54995aca749312a13b82fa41b730
Majora <121319119+MajoraMoon@users.noreply.github.com>
2024-12-12 14:49:55 +0100

    init commit - welcome

---
+++ src/mygame.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL konnte nicht initialisiert werden: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("LunaCore",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          800, 600, SDL_WINDOW_SHOWN);
+
+    if (!window)
+    {
+        printf("Fenster konnte nicht erstellt werden: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event e;
+    int quit = 0;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = 1;
+            }
+        }
+
+        SDL_Delay(16); // Begrenze die CPU-Auslastung etwas
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
