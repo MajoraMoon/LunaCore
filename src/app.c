c3fcf560439f8406c222a7d879f2c7f2d8116755
Majora <121319119+MajoraMoon@users.noreply.github.com>
2024-12-15 02:04:59 +0100

    First Prototype: FreeType Glyph rendering

--- src/app.c
+++ src/app.c
@@ -10,6 +10,64 @@ const unsigned int SCR_HEIGHT = 720;
 bool quit = false;
 float fps = 0;
 
+void RenderText(SDL_Renderer *renderer, FT_Face face, const char *text, int x, int y)
+{
+    // Set initial drawing position
+    int pen_x = x;
+    int pen_y = y;
+
+    // Going through every single character
+    for (const char *p = text; *p; p++)
+    {
+        // Loa character glyph (char) into FreeType
+        if (FT_Load_Char(face, *p, FT_LOAD_RENDER))
+        {
+            fprintf(stderr, "Failed to load glyph for character: %c\n", *p);
+            SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to load glyph for character: %c\n", *p);
+            continue;
+        }
+        // Access the glyph's bitmap
+        FT_Bitmap *bitmap = &face->glyph->bitmap;
+
+        // Create SDL surface from FreeType bitmap
+        SDL_Surface *surface = SDL_CreateRGBSurfaceWithFormatFrom(
+            bitmap->buffer,
+            bitmap->width,
+            bitmap->rows,
+            8,             // Bits per Pixel
+            bitmap->pitch, // row size
+            SDL_PIXELFORMAT_INDEX8);
+
+        // Map colors -> white text
+        SDL_Color colors[256] = {0};
+        colors[255].r = 255;
+        colors[255].g = 255;
+        colors[255].b = 255;
+        colors[255].a = 255;
+        SDL_SetPaletteColors(surface->format->palette, colors, 0, 256);
+
+        // Convert surface to texture
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+
+        // Set destination recangle on the screen
+        SDL_Rect dstrect = {
+            pen_x + face->glyph->bitmap_left,
+            pen_y - face->glyph->bitmap_top,
+            bitmap->width,
+            bitmap->rows};
+
+        // Render texture to the screen
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+        // Clean up texture
+        SDL_DestroyTexture(texture);
+
+        // Advance pen position
+        pen_x += face->glyph->advance.x >> 6;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     // First, initiate SDL. Here initiating sdl-subsystem
@@ -44,6 +102,34 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // Initiate FreeType to Render Fonts
+    FT_Library ft_lib;
+    if (FT_Init_FreeType(&ft_lib))
+    {
+        fprintf(stderr, "Could not initialize FreeType library\n");
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not initialize FreeType library\n");
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Load a Font with FreeType
+    FT_Face ft_face;
+    if (FT_New_Face(ft_lib, "../assets/fonts/Roboto/Roboto-Regular.ttf", 0, &ft_face))
+    {
+        fprintf(stderr, "Could not load font\n");
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Could not load font\n");
+        FT_Done_FreeType(ft_lib);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Set font size
+    FT_Set_Pixel_Sizes(ft_face, 0, 48);
+
     SDL_Event event;
 
     // activates V-sync. Similar to the GLFW function "glfwSwapInterval(int a)"
@@ -74,12 +160,17 @@ int main(int argc, char *argv[])
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
+        // Render Text
+        RenderText(renderer, ft_face, "Hello World!", 50, 100);
+
         SDL_RenderPresent(renderer);
 
         fps = calculateFPS_Float();
         SDL_Log("FPS: %.2f\n", fps);
     }
 
+    FT_Done_Face(ft_face);
+    FT_Done_FreeType(ft_lib);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
